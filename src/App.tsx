c83d19350938399d59ffd321bd3e48efdb4d055a
Diogo Peixoto <diogosoarespeixoto41@gmail.com>
2024-01-09 08:59:42 -0300

    create reducer patter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,73 @@
+import { useReducer } from "react";
 import { Container } from "./styles/styled";
 
+interface IActionProps {
+  type: "ADD_NUMBER" | "SUB_NUMBER" | "Mult_NUMBER";
+  payload: { number: number };
+}
+
 function App() {
-  return <Container></Container>;
+  //state = valor atual
+  const [number, dispatch] = useReducer(
+    (state: number, action: IActionProps) => {
+      if (action.type === "ADD_NUMBER") {
+        return state + action.payload.number;
+      }
+      if (action.type === "SUB_NUMBER") {
+        return state - action.payload.number;
+      }
+      if (action.type === "Mult_NUMBER") {
+        return state * action.payload.number;
+      }
+
+      return state;
+    },
+    0,
+  );
+
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD_NUMBER",
+      payload: {
+        number: 5,
+      },
+    });
+  };
+
+  const handleSub = () => {
+    dispatch({
+      type: "SUB_NUMBER",
+      payload: {
+        number: 1,
+      },
+    });
+  };
+
+  const handleMul = () => {
+    dispatch({
+      type: "Mult_NUMBER",
+      payload: {
+        number: 2,
+      },
+    });
+  };
+
+  return (
+    <Container>
+      <h1>
+        Usado para gerenciar dados complexos que ocorrem muitas alterações
+        custosas (com muito codigo) em diferentes lugares.
+      </h1>
+
+      <h3>Number: {number}</h3>
+
+      <div>
+        <button onClick={handleAdd}>+ 5</button>
+        <button onClick={handleMul}>* 2</button>
+        <button onClick={handleSub}>- 1</button>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
--- src/styles/styled.ts
+++ src/styles/styled.ts
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: #202024;
   color: white;
+  font-family: Arial, Helvetica, sans-serif;
 
-  width: 100vw;
-  height: 100vh;
+  max-width: 90%;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
+
+  button {
+    height: 30px;
+    width: 150px;
+    margin-inline: 5px;
+    border-radius: 5px;
+    background-color: white;
+    font-size: 14px;
+  }
 `;
